96545dba2587d9227f92af19c42bca14f1c5d80b
hejiang <18715770814@163.com>
2020-12-04 11:20:54 +0800

    1. 项目初始化，引入项目依赖

---
+++ src/main/java/com/ql/qlsql/QlSqlApplication.java
@@ -0,0 +1,13 @@
+package com.ql.qlsql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QlSqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QlSqlApplication.class, args);
+    }
+
+}
